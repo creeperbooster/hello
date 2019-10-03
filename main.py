d56970b6af94bf4f911bc27d599c85b06ddea718
Maxim <max1361@yandex.ru>
2019-10-03 15:02:11 +0300

    Commit4

--- main.py
+++ main.py
@@ -9,10 +9,12 @@ subprocess.call(["git", "clone", "https://github.com/creeperbooster/hello"])
 
 #subprocess.call(["git", "pull"])
 #subprocess.call(["git", "add", "main.cpp"])
-#subprocess.call(["git", "commit", "-m", "Commit3"])
+
 
 
 #subprocess.call(["git", "push", "https://github.com/creeperbooster/hello"])
 subprocess.call(["git", "add", "."])
+subprocess.call(["git", "commit", "-m", "Commit4"])
+
 print("Check")
 
